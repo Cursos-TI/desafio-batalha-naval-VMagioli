cbc48660be6d2f0dd5f9285024ab9ed6909c00f3
VMagioli <202408555954@alunos.estacio.br>
2024-10-26 22:27:20 +0000

    alteração for exibição

--- desafioaventureiro.c
+++ desafioaventureiro.c
@@ -15,14 +15,5 @@ int main(){
             scanf("%d", &matriz[i][j]);
         }
     }
-
-    // Exibe o tabuleiro com posicionamento dos navios preenchido
-    printf("\nA matriz preenchida é:\n");
-    for (int i = 0; i < LINHAS; i++) {
-        for (int j = 0; j < COLUNAS; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");  // Nova linha após cada linha da matriz
-    }
     return 0;
 }
