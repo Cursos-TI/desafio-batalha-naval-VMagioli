bb9fd0cac68aa1ff0a584651ff65b33ed37c8c7e
VMagioli <202408555954@alunos.estacio.br>
2024-10-26 22:25:53 +0000

    insere for para solicitação de preencher matriz

---
+++ desafioaventureiro.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define LINHAS 10
+#define COLUNAS 10
+
+int main(){
+
+    int matriz [LINHAS] [COLUNAS]; // Declara a matriz com o tamanho definido nas constantes
+
+    //Solicita ao usuário montar o tabuleiro com posicionamento dos navios
+    printf("Digite os valores da matriz:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    // Exibe o tabuleiro com posicionamento dos navios preenchido
+    printf("\nA matriz preenchida é:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");  // Nova linha após cada linha da matriz
+    }
+    return 0;
+}
