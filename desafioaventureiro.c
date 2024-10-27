17a745f70ceb21a3432bd8e90f892bfe5d1dc16d
VMagioli <202408555954@alunos.estacio.br>
2024-10-27 23:34:22 +0000

    inserindo valores da matriz manualmente

--- desafioaventureiro.c
+++ desafioaventureiro.c
@@ -5,16 +5,19 @@
 
 int main(){
 
-    int matriz [LINHAS] [COLUNAS]; // Declara a matriz com o tamanho definido nas constantes
+    int matriz [LINHAS] [COLUNAS]= {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {3, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+        {0, 3, 0, 0, 0, 0, 3, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 3, 0, 0, 0},
+        {0, 0, 0, 3, 0, 0, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 3, 3, 3, 3, 0, 0, 0, 3, 0},
+        {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+    }; // Declara a matriz com o tamanho definido nas constantes
 
-    //Solicita ao usuário montar o tabuleiro com posicionamento dos navios
-    printf("Posicione os navios no tabuleiro:\n");
-    for (int i = 0; i < LINHAS; i++) {
-        for (int j = 0; j < COLUNAS; j++) {
-            printf("Elemento [%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
-        }
-    }
     // Exibe o tabuleiro com posicionamento dos navios preenchido
     printf("\nVeja como ficou o tabuleiro:\n");
     for (int i = 0; i < LINHAS; i++) {
